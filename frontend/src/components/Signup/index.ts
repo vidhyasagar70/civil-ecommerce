ecc333bdaf2a97381896b4006712a2dc35c2ffd5
Meenatechjays <meena.sivakumar@techjays.com>
2025-09-11 23:47:51 +0530

    user registration ui created

---
+++ frontend/src/components/Signup/index.ts
@@ -0,0 +1,4 @@
+export { default as SocialLoginButtons } from './SocialLoginButtons';
+export { default as SignupForm } from './SignupForm';
+export { default as PasswordInput } from './PasswordInput';
+export { default as FormFooter } from './FormFooter';
