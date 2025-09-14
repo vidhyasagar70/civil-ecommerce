075b00675a306420e26ef9044f17d4c47712bd38
vidhyasagar70 <vidhyas369@gmail.com>
2025-09-14 10:56:45 +0530

    vercel deployement

--- backend/server.ts
+++ backend/server.ts
@@ -13,17 +13,20 @@ app.use(express.json());
 
 app.use("/api/products", productRoutes);
 
-const PORT = process.env.PORT || 5000;
+app.get("/", (req, res) => {
+  res.send("Backend running on Vercel 🚀");
+});
 
+// MongoDB connection
 mongoose
   .connect(process.env.MONGODB_URI as string)
   .then(() => {
     console.log("MongoDB connected");
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
-    });
   })
   .catch((err) => {
-    console.error(" MongoDB connection error:", err);
-    process.exit(1); 
+    console.error("MongoDB connection error:", err);
+    process.exit(1);
   });
+
+
+export default app;
