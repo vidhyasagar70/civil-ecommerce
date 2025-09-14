995b63007c30b6ad7808b05fd7daa1f318601f59
vidhyasagar70 <vidhyas369@gmail.com>
2025-09-14 11:01:34 +0530

    vercel

--- backend/server.ts
+++ backend/server.ts
@@ -1,4 +1,4 @@
-import express, { Application } from "express";
+import express, { Application, Request, Response } from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
@@ -11,22 +11,21 @@ const app: Application = express();
 app.use(cors());
 app.use(express.json());
 
+
 app.use("/api/products", productRoutes);
 
-app.get("/", (req, res) => {
+app.get("/", (req: Request, res: Response) => {
   res.send("Backend running on Vercel 🚀");
 });
 
-// MongoDB connection
+
 mongoose
   .connect(process.env.MONGODB_URI as string)
   .then(() => {
     console.log("MongoDB connected");
   })
   .catch((err) => {
-    console.error("MongoDB connection error:", err);
-    process.exit(1);
+    console.error(" MongoDB connection error:", err);
   });
-
 
 export default app;
