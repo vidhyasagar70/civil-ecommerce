77f5bd875b7ee68f63601d0e3219ac3c010124b4
Umarfarook <umarfarookj06@gmail.com>
2025-10-24 15:33:14 +0530

    cors error fixed

--- backend/server.ts
+++ backend/server.ts
@@ -22,7 +22,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || "http://localhost:5173" || "https://civil-ecommerce-yiav.vercel.app",
     credentials: true,
   })
 );
